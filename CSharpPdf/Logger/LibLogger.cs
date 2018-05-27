2b205a04b9602ca4854dd0f699cf963ffc57f098
Adam Adamczyk <adam@findox.com>
2018-05-27 21:24:44 +0200

    logger more func

--- CSharpPdf/Logger/LibLogger.cs
+++ CSharpPdf/Logger/LibLogger.cs
@@ -14,5 +14,24 @@ namespace CSharpPdf.Logger
             LogManager.GetLogger(type.FullName).Debug(text);
         }
 
+        public static void Error(Type type, string text)
+        {
+            LogManager.GetLogger(type.FullName).Error(text);
+        }
+
+        public static void Warn(Type type, string text)
+        {
+            LogManager.GetLogger(type.FullName).Warn(text);
+        }
+
+        public static void Trace(Type type, string text)
+        {
+            LogManager.GetLogger(type.FullName).Trace(text);
+        }
+
+        public static void Fatal(Type type, string text)
+        {
+            LogManager.GetLogger(type.FullName).Fatal(text);
+        }
     }
 }
